3d002d7153786a4fbef3030482f21f3db0b1b4c4
Mark Grimes <Mark Grimes>
2016-11-06 17:53:23 +0000

    Implemented jumbotron in posts page.

--- static/posts.js
+++ static/posts.js
@@ -1,39 +1,17 @@
-//if a paragraph element is clicked
-/*$("p").click(function () {
-    var word = $(this).text();
-
-    word = word.replace(/\n/g, "<br/>");
-
-    var id = $(this).attr("id")
-    console.log(id)
-    $(this).html(word);
-//test out to see if i can format on page load
-//placed into a function that loops through all entries
-
-}
-
-
-);*/
-
-
-
-
-
-
 $(function () {
-    // var posts = $("#postContainer").children("p").text();
+    
     console.log("triggering function")
-    //console.log(posts)
-    //get length
-    var posts = $("#postContainer").children("p");
+   
+    var jumboCount = $("#postContainer").children("div div ");
+    console.log(jumboCount)
 
     //get the children of the post container that are paragraph elements
     //trigger a function
-    count = 0;
-    for (var i = 0; i < posts.length; i++) {
-        console.log(posts[i].id)
+
+    for (var i = 0; i < jumboCount.length; i++) {
+        console.log(jumboCount[i].id)
         //now get the p
-        var old = $("#" + posts[i].id).text();
+        var old = $("#" + jumboCount[i].id).find("p").text();
         //grab the text of each post by id
         var update = old.replace(/\n/g, "<br/>");
 
@@ -46,8 +24,8 @@ $(function () {
         //hurray
         console.log(update)
         //now update the view
-        $("#" + posts[i].id).html(update);
-        count++
+        $("#" + jumboCount[i].id).find("p").html(update);
+ 
 
         //var word = post.textContent();
         // word= word.replace(/\n/g, "<br/>");
